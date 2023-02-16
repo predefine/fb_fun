33bf4e9c7300d003838d6757a11cb39c20895a92
Leandro Friedrich <email@leandrofriedrich.de>
2023-02-16 21:47:15 +0100

    add simplefb, borrowed from uniLoader

--- startup.s
+++ startup.s
@@ -5,9 +5,9 @@ _Reset:
  BIC r0, r0, #0x40
  BIC r0, r0, #0x200000
  MCR p15, 0, r0, c1, c0, 1
- ldr     r4, =0xa9000864        @ bank6_in (phys)
- ldr     r5, =0xa9000814        @ bank6_out (phys)
- orr     r6, r4, #0x200000      @ 22nd bit for flash
- str     r6, [r5, #0]           @ store in out (enables bright for 500ms, limited by hardware)
+ ldr r4, =0xa9000864
+ ldr     r5, =0xa9000814
+ orr     r6, r4, #0x200000
+ str     r6, [r5, #0]
  BL c_entry
  B .
--- test.c
+++ test.c
@@ -1,9 +1,85 @@
-#include <stdio.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Ivaylo Ivanov <ivo.ivanov.ivanov1@gmail.com>
+ * Copyright (c) 2022, Markuss Broks <markuss.broks@gmail.com>
+ * Copyright (c) 2022, Michael Srba <Michael.Srba@seznam.cz>
+ * Copyright (c) 2023, Leandro Friedrich <email@leandrofriedrich.de>
+ */
+
 #include <stdint.h>
-uint8_t *start = (uint8_t *)0x2a00000;
-uint8_t *end = (uint8_t *)0x2abbb00;
+#include "simplefb.h"
+#include "font.h"
+#include "debug.h"
+void clean_fb(volatile char *fb, char *text, int width, int height, int stride) {
+        for (volatile char *addr = fb; addr < fb + (width * height * stride); addr += stride)
+                *(int*) (addr) = 0x0;
+}
+
+void draw_pixel(volatile char *fb, int x, int y, int width, int stride) {
+        long int location = (x * stride) + (y * width * stride);
+
+        *(fb + location) = 255; // Blue
+        *(fb + location + 1) = 255;     // Green
+        *(fb + location + 2) = 255;     // Red
+        *(fb + location + 3) = 255;     // Full opacity
+}
+
+void draw_horizontal_line(volatile char *fb, int x1, int x2, int y, color c, int width, int stride) {
+        for (int i = x1; i < x2; i++)
+                draw_pixel(fb, i, y, width, stride);
+}
+
+void draw_vertical_line(volatile char *fb, int x, int y1, int y2, color c, int width, int stride) {
+        for (int i = y1; i < y2; i++)
+                draw_pixel(fb, x, i, width, stride);
+}
+
+void draw_filled_rectangle(volatile char *fb, int x1, int y1, int w, int h, color c, int width, int stride) {
+        for (int i = y1; i < (y1 + h); i++)
+                draw_horizontal_line(fb, x1, (x1 + w), i, c, width, stride);
+}
+
+void draw_text(volatile char *fb, char *text, int textX, int textY, int width, int stride) {
+        // loop through all characters in the text string
+        int l = strlen(text);
+
+        for (int i = 0; i < l; i++) {
+                if(text[i] < 32)
+                        continue;
+
+                int ix = font_index(text[i]);
+                unsigned char *img = letters[ix];
+
+                for (int y = 0; y < FONTH; y++) {
+                        unsigned char b = img[y];
+
+                        for (int x = 0; x < FONTW; x++) {
+                                if (((b << x) & 0b10000000) > 0)
+                                        draw_pixel(fb, textX + i * FONTW + x, textY + y, width, stride);
+                        }
+                }
+        }
+}
+
+/* Helper functions */ 
+font_params get_font_params() {
+        font_params params = {.width=FONTW, .height=FONTH};
+
+        return params;
+}
+
+void printk(char *text) {
+	if(debug_linecount > 100 || debug_linecount < 0)
+		debug_linecount = 0;
+
+//	draw_text((char*)0x2a00000, "[uniLoader] ", 0, (20 + (debug_linecount * 30)), 480, 4);
+	draw_text((char*)0x2a00000, text, 0, (20 + (debug_linecount * 30)), 480, 4);
+
+	debug_linecount++;
+}
+
 int c_entry() {
-	for (uint8_t *ptr = start; ptr < end; ptr++) {
-    	*ptr = 255;
+	for(;;) {
+		printk("hi\n");
 	}
 }
